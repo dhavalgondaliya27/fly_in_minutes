0f76db4cfdef7e7972c6f7e9b9aa58fad82e0b16
Bhavin karena <bhavinkarena2003@gmail.com>
2025-04-19 01:39:25 +0530

    Solve error

--- src/app.js
+++ src/app.js
@@ -5,21 +5,10 @@ import helmet from 'helmet';
 import compression from 'compression';
 import ResponseEnhancer from './utils/responseEnhancer.js';
 import mainRoute from './routes/index.js';
-
+import session from 'express-session';
 const app = express();
-app.use(
-  cors({
-    origin: [
-      'http://localhost:3000',
-    ],
-    credentials: true,
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
-  })
-);
 
-app.options('*', cors());
-
+app.use(cors());
 app.use(
   session({
     secret: 'FlySecret',
@@ -32,17 +21,13 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
 app.use(ResponseEnhancer);
 app.get('/', (req, res) => {
   res.send('Fly is running ...');
 });
-mainRoute(app)
-app.use((req, res, next) => {
-  res.error(404, 'Route not found');
-});
+app.use(mainRoute);
+
 app.use(helmet());
 app.use(compression());
-
 
 export { app };
