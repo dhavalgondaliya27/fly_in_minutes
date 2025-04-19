c3592992d813d45265880088532ab3cfa3864ce5
dhavalgondaliya27 <bhavnagondaliya77@gmail.com>
2025-04-19 16:37:26 +0530

    Project setup done

---
+++ src/api/route.js
@@ -0,0 +1,8 @@
+const apiVersion = '/api/v1';
+
+const mainRoutes = app => {
+  //   //for authentication with email, gooogle and identity verification
+  //   app.use(apiVersion, UserRouter);
+};
+
+export default mainRoutes;
