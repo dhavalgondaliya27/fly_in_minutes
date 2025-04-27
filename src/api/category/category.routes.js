40344a03f65bdb4de0652eb6e2cc08575f6e8e76
bhavinkarena <bhavinkarena2003@gmail.com>
2025-04-27 12:45:21 +0530

    remove array

--- src/api/category/category.routes.js
+++ src/api/category/category.routes.js
@@ -10,10 +10,10 @@ import { checkRole, verifyJWT } from '../../middlewares/auth.middleware.js';
 
 const categoryRouter = Router();
 
-categoryRouter.post('/create', checkRole(['admin']), createCategory);
+categoryRouter.post('/create', checkRole('admin'), createCategory);
 categoryRouter.get('/all', getAllCategories);
 categoryRouter.get('/:categoryId', getCategoryById);
-categoryRouter.put('/update/:categoryId', checkRole(['admin']), updateCategory);
-categoryRouter.delete('/delete/:categoryId', checkRole(['admin']), deleteCategory);
+categoryRouter.put('/update/:categoryId', checkRole('admin'), updateCategory);
+categoryRouter.delete('/delete/:categoryId', checkRole('admin'), deleteCategory);
 
 export default categoryRouter;
